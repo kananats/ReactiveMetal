2494660b73c92361a41bc2cb4ea4b9372cc3b2b8
s.kananat <s.kananat@donuts.ne.jp>
2018-12-18 16:47:18 +0900

    add comment

--- ReactiveMetal/ImageTarget/RenderView.swift
+++ ReactiveMetal/ImageTarget/RenderView.swift
@@ -22,6 +22,7 @@ public final class RenderView: UIView {
     let vertexBuffer: MTLBuffer
     let indexBuffer: MTLBuffer
     
+    /// Texture
     private var texture: MTLTexture?
 
     /// Metal view
