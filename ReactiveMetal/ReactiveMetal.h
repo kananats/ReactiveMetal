53705f33ac4d06cea0f3f065d6c9ec1bc0d8049f
s.kananat <s.kananat@donuts.ne.jp>
2018-12-18 14:32:46 +0900

    make framework

---
+++ ReactiveMetal/ReactiveMetal.h
@@ -0,0 +1,19 @@
+//
+//  ReactiveMetal.h
+//  ReactiveMetal
+//
+//  Created by s.kananat on 2018/12/18.
+//  Copyright © 2018 s.kananat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ReactiveMetal.
+FOUNDATION_EXPORT double ReactiveMetalVersionNumber;
+
+//! Project version string for ReactiveMetal.
+FOUNDATION_EXPORT const unsigned char ReactiveMetalVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ReactiveMetal/PublicHeader.h>
+
+
